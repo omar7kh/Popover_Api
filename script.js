7cbe3db5659e5d7e0e1835f8496ffa1c7343a695
Omar Alkhaled <omarkh1997aled@gmail.com>
2024-06-10 00:13:47 +0200

    method to set up popovers and handle unsupported browser

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ const supportsPopover = () => {
 };
 
 const popoverSupported = supportsPopover();
+
+const configurePopovers = () => {
+  if (popoverSupported) {
+    popovers.forEach((popover, i) => {
+      popover.popover = 'auto';
+      toggleButtons[i].popoverTargetElement = popover;
+      toggleButtons[i].addEventListener = 'toggle';
+    });
+  } else {
+    toggleButtons.forEach((button) => (button.innerHTML = 'Not Supported'));
+    popovers.forEach((button) => (button.style.display = 'none'));
+  }
+};
+
+configurePopovers();

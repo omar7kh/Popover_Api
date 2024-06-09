ef3a9238f73f4c523eda5a09e5ac69aabda59c95
Omar Alkhaled <omarkh1997aled@gmail.com>
2024-06-10 00:11:06 +0200

    script to handle toggle buttons and popover elements

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggleButtons = [
+  document.querySelector('#toggleButton1'),
+  document.querySelector('#toggleButton2'),
+];
+
+const popovers = [
+  document.querySelector('#popover1'),
+  document.querySelector('#popover2'),
+];
